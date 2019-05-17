47a573f57b4ef76391e4d8393a2eafa99eff575a
huangzhong <huangzhong@bytedance.com>
2019-05-17 20:32:33 +0800

    feat: fix

--- src/tree.js
+++ src/tree.js
@@ -7,14 +7,14 @@ class Leaf {
 }
 
 let preKey = '';
-function prefixKey(key) {
-  preKey = preKey ? `${preKey}${typeof key === 'number' ? `[${key}]` : `.${key}`}` : key;
+function prefixKey(key, tree) {
+  preKey = preKey ? `${preKey}${tree.isArray ? `[${key}]` : `.${key}`}` : key;
   return preKey;
 }
 
 function getValueFactory(result) {
-  return (key, value) => {
-    result[prefixKey(key)] = value; // eslint-disable-line no-param-reassign
+  return (key, value, tree) => {
+    result[prefixKey(key, tree)] = value; // eslint-disable-line no-param-reassign
     preKey = '';
   };
 }
@@ -30,7 +30,10 @@ export default class Tree {
     for (let i = 0, l = keys.length - 1; i < l; i++) {
       const key = keys[i];
       if (cur.constructor === Tree) {
-        !cur.members[key] && (cur.members[key] = new Tree(typeof key === 'number'));
+        if (!cur.members[key]) {
+          cur.isArray = typeof key === 'number';
+          cur.members[key] = new Tree();
+        }
         cur = cur.members[key];
       } else { // todo
         cur = cur[key];
@@ -56,28 +59,22 @@ export default class Tree {
   }
 
   merge(data) {
-    let result = data;
-    const keys = [];
+    const result = { ...data };
+    let tem = result;
     this.walk(
       (k, v) => {
-        result = { ...result };
-        let obj = result;
-        for (let i = 0, l = keys.length; i < l; i++) {
-          const key = keys[i];
-          obj[key] = { ...obj[key] };
-          obj = obj[key];
-        }
-        console.log(v);
-        obj[k] = v;
+        tem[k] = v;
+        tem = result;
       },
       (k, tree) => {
-        keys.push(tree.isArray ? Number(k) : k);
+        tem[k] = tree.isArray ? tem[k].slice() : { ...tem[k] };
+        tem = tem[k];
       },
     );
     return result;
   }
 
-  walk(onLeaf = noop, onTree = noop) {
+  walk(onLeaf = noop, onTree = noop) { // todo
     const keys = Object.keys(this.members);
     for (let i = 0, l = keys.length; i < l; i++) {
       const key = keys[i];
@@ -86,7 +83,7 @@ export default class Tree {
         onTree(key, this);
         child.walk(onLeaf, onTree);
       } else {
-        onLeaf(key, child.value);
+        onLeaf(key, child.value, this);
       }
     }
   }
